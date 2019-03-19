b209b4cd8ac560236b394b510519d013ad0cab8c
Elchr <elchr21@gmail.com>
2019-03-19 21:28:01 +0200

    Update RandomWalks.m

--- RandomWalks.m
+++ RandomWalks.m
@@ -1,40 +1,39 @@
 clc; clear;
 
- %Προσομοίωση Monte Carlo για σωματίδια σε χώρο 1Δ και τυχαία κίνηση αριστερά-δεξιά
- %Random Walks - Monte Carlo
+ %Random Walks 1D - Monte Carlo
  
-for n=1:100000              %Δημιουργία 100000 σωματιδίων
-     thesi(n)=0;            %Αρχική θέση σωματιδίων (αρχή των αξόνων)
-       for i=1:10           %Δημιουργία 10 βημάτων για ένα σωματίδιο
-          x=rand();         %Γέννηση ψευδοτυχαίου αριθμού για τη φορά κίνησης σε κάθε βήμα
+for n=1:100000              %Creation of 100000 particles
+     thesi(n)=0;            %Position in the begining (0,0)
+       for i=1:10           %Each particle moves 10 steps
+          x=rand();         %Random number generation for the direction of the particle (left or right?)
                  if x<0.5 
-                    thesi(n)=thesi(n)-0.5;    %Κίνηση προς την αρνητική φορά του άξονα x
+                    thesi(n)=thesi(n)-0.5;    %Move left
                  elseif x>=0.5
-                    thesi(n)=thesi(n)+0.5;    %Κίνηση προς την θετική φορά του άξονα x
+                    thesi(n)=thesi(n)+0.5;    %Move right
                  end
            i=i+1;
        end
       n=n+1;
 end
 
-histogram(thesi) %Δημιουργία ιστογράμματος (αριθμός σωματιδίων - τελική θέση σωματιδίων)
+histogram(thesi) %Histogram (number of partcles - final position) 
 title('Random Walks - Monte Carlo');
 xlabel('Displacement x');
 ylabel('Number of particles');
 
 hold on
-for n=1:100000               %Δημιουργία 100000 σωματιδίων
-     thesi2(n)=0;            %Αρχική θέση σωματιδίων (αρχή των αξόνων)
-       for i=1:100           %Δημιουργία 100 βημάτων για ένα σωματίδιο
-          x=rand();          %Γέννηση ψευδοτυχαίου αριθμού για τη φορά κίνησης σε κάθε βήμα
+for n=1:100000               
+     thesi2(n)=0;            
+       for i=1:100           %Each particle moves 10 steps
+          x=rand();          
                  if x<0.5 
-                    thesi2(n)=thesi2(n)-0.5;    %Κίνηση προς την αρνητική φορά του άξονα x
+                    thesi2(n)=thesi2(n)-0.5;    
                  elseif x>=0.5
-                    thesi2(n)=thesi2(n)+0.5;    %Κίνηση προς την θετική φορά του άξονα x
+                    thesi2(n)=thesi2(n)+0.5;   
                  end
            i=i+1;
        end
       n=n+1;
 end
-histogram(thesi2) %Συμπλήρωση ιστογράμματος 
+histogram(thesi2)
 legend('10 random moves/particle','100 random moves/particle');
